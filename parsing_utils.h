dc60b54a0cc337c500d5c7ac72213591972de6b6
Jonathan Simpson <jsimpson.github@gmail.com>
2017-10-13 19:57:36 -0700

    clean up

--- parsing_utils.h
+++ parsing_utils.h
@@ -3,7 +3,9 @@
 
 int xml_content_to_i(xmlDocPtr document, xmlNodePtr node);
 double xml_content_to_d(xmlDocPtr document, xmlNodePtr node);
+
 lap_t * parse_lap(xmlDocPtr document, xmlNsPtr ns, xmlNodePtr node);
+
 void parse_trackpoint_coordinates(trackpoint_t * trackpoint, xmlDocPtr document, xmlNsPtr ns, xmlNodePtr node);
 void parse_trackpoint_heart_beat(trackpoint_t * trackpoint, xmlDocPtr document, xmlNsPtr ns, xmlNodePtr node);
 void parse_trackpoint_extensions(trackpoint_t * trackpoint, xmlDocPtr document, xmlNodePtr node);
